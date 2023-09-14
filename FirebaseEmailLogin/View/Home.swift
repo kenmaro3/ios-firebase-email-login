3b815cf76edbe6bb4aa6dc482719a6f9725579d6
kenmaro3 <miha.ken.19@gmail.com>
2023-09-14 11:24:17 +0900

    [ADD] biometrics and logout, delete

--- FirebaseEmailLogin/View/Home.swift
+++ FirebaseEmailLogin/View/Home.swift
@@ -1,9 +1,47 @@
 
 import SwiftUI
+import FirebaseAuth
 
 struct Home: View {
+    @AppStorage("loginStatus") var app_storage_login_status: Bool = false
+    @AppStorage("isUseBiometrics") var is_use_biometrics: Bool = false
+    @AppStorage("name") var app_storage_name: String = ""
+    @AppStorage("email") var app_storage_email: String = ""
+    @AppStorage("password") var app_storage_password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, \(app_storage_name)")
+        
+        Button(action: {
+            let firebaseAuth = Auth.auth()
+            do {
+                try firebaseAuth.signOut()
+                app_storage_login_status = false
+            } catch let signOutError as NSError {
+                print("Error signing out: %@", signOutError)
+            }
+            
+        }, label: {
+            Text("ログアウト")
+        }).padding()
+        
+        Button(action: {
+            let user = Auth.auth().currentUser
+            
+            user?.delete { error in
+                if let error = error {
+                    // エラー発生
+                } else {
+                    // 退会成功
+                    app_storage_login_status = false
+                    is_use_biometrics = false
+                    app_storage_name = ""
+                    app_storage_email = ""
+                    app_storage_password = ""
+                }
+            }
+        }, label: {
+            Text("アカウントの削除")
+        }).padding()
     }
 }
 
--- FirebaseEmailLogin/View/Login.swift
+++ FirebaseEmailLogin/View/Login.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 import FirebaseAuth
+import LocalAuthentication
 
 struct AuthenticationView: View {
     @AppStorage("loginStatus") var app_storage_login_status: Bool = false
+    @AppStorage("isUseBiometrics") var is_use_biometrics: Bool = true
+    @AppStorage("name") var app_storage_name: String = ""
+    @AppStorage("email") var app_storage_email: String = ""
+    @AppStorage("password") var app_storage_password: String = ""
+    @State var isUseBiometrics: Bool = true
     
     @State  var email:String = ""
     @State  var password:String = ""
@@ -15,17 +21,15 @@ struct AuthenticationView: View {
                 TextField("password", text: $password).padding().textFieldStyle(.roundedBorder)
                 
                 Button(action: {
-                    Auth.auth().signIn(withEmail: email, password: password) { result, error in
-                        if let user = result?.user {
-                            // ログイン時に閲覧できる画面として画面遷移させる
-                            print("ログインしました。")
-                            app_storage_login_status = true
-                        }
-                    }
+                    firebaseEmailLogin()
                   
                 }, label: {
                     Text("ログイン")
                 }).padding()
+                
+                Toggle(isOn: $isUseBiometrics) {
+                    Text(isUseBiometrics ? "次回から生体認証ログインを使用する" : "生体認証ログインは使用しない")
+                }.padding()
                 
                 NavigationLink(destination: PasswordResetView(), label: {
                     Text("パスワードリセット")
@@ -35,6 +39,64 @@ struct AuthenticationView: View {
                     Text("未登録の方はこちら")
                 })
             }
+            .onChange(of: isUseBiometrics){ newValue in
+                is_use_biometrics = isUseBiometrics
+            }
+            .onAppear{
+                print("here")
+                Task.detached{ @MainActor in
+                    print("will start on appear main")
+                    if(is_use_biometrics){
+                        faceIdAuthentication()
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    func firebaseEmailLogin(){
+        
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let user = result?.user {
+                // ログイン時に閲覧できる画面として画面遷移させる
+                print("ログインしました。")
+                app_storage_login_status = true
+                app_storage_name = user.displayName!
+                app_storage_email = email
+                app_storage_password = password
+                is_use_biometrics = isUseBiometrics
+                
+            }
+        }
+    }
+    
+    func faceIdAuthentication(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "Authenticate to access the app"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
+                if success{
+                    print("successed")
+                    Auth.auth().signIn(withEmail: app_storage_email, password: app_storage_password) { result, error in
+                        if let user = result?.user {
+                            // ログイン時に閲覧できる画面として画面遷移させる
+                            print("ログインしました。")
+                            app_storage_login_status = true
+                            app_storage_name = user.displayName!
+                            
+                        }
+                    }
+                }else{
+                    print("failed")
+                    app_storage_login_status = false
+                }
+            }
+        }else{
+            // Device does not support Face ID or Touch ID
+            print("Biometric authentication unavailable")
         }
     }
 }
