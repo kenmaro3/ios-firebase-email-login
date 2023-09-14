40a38d3ae70377d74279599f68103a828d1251c0
kenmaro3 <miha.ken.19@gmail.com>
2023-09-14 10:14:03 +0900

    Initial Commit

---
+++ FirebaseEmailLogin/FirebaseEmailLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseEmailLoginApp.swift
+//  FirebaseEmailLogin
+//
+//  Created by Kentaro Mihara on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseEmailLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
